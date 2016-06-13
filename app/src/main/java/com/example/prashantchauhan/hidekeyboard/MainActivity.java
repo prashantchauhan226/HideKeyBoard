ffbd1d608ad066d85f2a02d1a6b1004a08d4533d
Prashant Chauhan <invisible@226>
2016-06-13 14:17:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/prashantchauhan/hidekeyboard/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.prashantchauhan.hidekeyboard;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText editText = (EditText) findViewById(R.id.et_1);
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.line1);
+        linearLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent ev) {
+                hideKeyboard(view);
+                return false;
+            }
+        });
+
+    }
+
+    protected void hideKeyboard(View view) {
+        InputMethodManager in = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        in.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+    }
+}
